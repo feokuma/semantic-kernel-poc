f7e6d21d3ddfe7f59402b3f1e32e062bee4a1117
Fernando Okuma <feokuma@gmail.com>
2024-07-07 15:45:33 -0300

    ✨ Implementa integração básica com OpenAI

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.SemanticKernel;
+
+var apiKey = Environment.GetEnvironmentVariable("OPENAPI_KEY");
+
+var builder = Kernel.CreateBuilder();
+builder.AddOpenAIChatCompletion("gpt-4", apiKey!);
+var kernel = builder.Build();
+
+string request = "Quantos tipos de orquideas existem?";
+string prompt = $"What is the intent of this request? {request}";
+
+Console.WriteLine(await kernel.InvokePromptAsync(prompt));
+Console.WriteLine("-------------------------");
